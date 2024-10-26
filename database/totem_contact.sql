055ef59655afd09f6a14d06ddf6a6640e6c45395
Iacob Alexandru <iacobalex11@yahoo.com>
2024-10-26 16:43:02 +0300

    final 3

--- database/totem_contact.sql
+++ database/totem_contact.sql
@@ -27,28 +27,25 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   `CNP` varchar(13) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `birth_date` date DEFAULT NULL,
   `email` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `phone` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table totem_contact.contacts: ~13 rows (approximately)
-INSERT INTO `contacts` (`id`, `name`, `gender`, `CNP`, `birth_date`, `email`, `phone`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, 'Micheal Prosacco', 'female', '7310680376861', '1973-09-07', 'flavio24@example.net', '5522309873', '2022-11-29 18:21:49', '2024-10-26 10:35:22', NULL),
-	(2, 'London Sawayn', 'female', '6813606596192', '2007-07-13', 'jennings.davis@example.net', '0245289863', '1980-01-08 01:20:25', NULL, NULL),
-	(3, 'Keshawn Dietrich', 'male', '8705515158329', '2018-07-13', 'ryan.alia@example.org', '2007035355', '1975-06-16 11:45:25', NULL, NULL),
-	(4, 'Miracle Daugherty', 'male', '1344725397717', '1978-12-27', 'trent.dicki@example.com', '1747928494', '1971-10-18 08:47:11', NULL, NULL),
-	(5, 'Prince Wuckert', 'male', '0023933088846', '1973-12-29', 'keagan05@example.org', '4549358467', '1997-05-22 11:23:38', NULL, NULL),
-	(6, 'Idell Aufderhar', 'female', '3456210045917', '1993-01-26', 'kautzer.gregory@example.org', '0573949330', '2005-07-25 07:32:36', NULL, NULL),
-	(7, 'Adonis Kuhlman', 'female', '9677026969581', '2021-06-27', 'barrows.norma@example.org', '1012646903', '2016-03-21 20:59:39', NULL, NULL),
-	(8, 'Ward Dach', 'female', '1907978825455', '1975-12-14', 'bemmerich@example.org', '2307670610', '2010-01-08 01:53:53', NULL, NULL),
-	(9, 'Mateo Buckridge I', 'female', '6756165508307', '2004-10-30', 'owen54@example.net', '1211541988', '1997-05-31 08:30:28', NULL, NULL),
-	(10, 'Merlin Hartmann', 'female', '8689957143221', '2004-05-20', 'ronny.hilpert@example.net', '5942458492', '1980-06-13 06:12:54', NULL, NULL),
-	(11, 'Iacob Alexandru-Georgian', 'male', '1111111111111', '2024-10-15', 'iacobalex11@yahoo.com', '0758848066', '2024-10-26 08:19:01', '2024-10-26 10:35:13', NULL),
-	(12, 'Iacob Alexandru-Georgian22', 'male', '1111111111211', '2024-10-16', 'iacobalex11@yahoo.com', '0758848066', '2024-10-26 08:22:38', '2024-10-26 10:35:03', NULL),
-	(13, 'Iacob Alexandru-Georgian44', 'female', '1111111111133', '2024-10-14', 'iacobalex11@yahoo.com', '0758848066', '2024-10-26 08:28:59', '2024-10-26 10:36:44', '2024-10-26 10:36:44');
+-- Dumping data for table totem_contact.contacts: ~10 rows (approximately)
+INSERT INTO `contacts` (`id`, `name`, `gender`, `CNP`, `birth_date`, `email`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'Amos Bashirian', 'male', '0750239870105', '2018-02-20', 'zander.ferry@example.org', '1998-07-03 13:38:37', NULL, NULL),
+	(2, 'Benedict Heathcote', 'male', '1439535543786', '2008-11-26', 'aniyah62@example.org', '2015-03-09 20:27:04', '2024-10-26 12:06:36', '2024-10-26 12:06:36'),
+	(3, 'Addie Bogisich', 'male', '8044426633477', '2006-11-07', 'alex.gulgowski@example.net', '1985-05-19 13:06:56', NULL, NULL),
+	(4, 'Ms. Anne Haley Sr.', 'female', '6034398497434', '2008-06-23', 'wtowne@example.org', '1997-08-22 04:48:37', NULL, NULL),
+	(5, 'Cayla Bogan', 'female', '3227243876766', '2020-07-10', 'qmueller@example.net', '1988-01-03 02:43:01', NULL, NULL),
+	(6, 'Ella O\'Hara MD', 'male', '0580525165418', '2023-07-26', 'edwardo36@example.com', '2007-01-12 14:57:06', NULL, NULL),
+	(7, 'Celine Kihn Sr.23', 'male', '8754722729497', '1976-03-18', 'tconnelly@example.org', '2022-08-18 17:54:29', '2024-10-26 13:31:31', '2024-10-26 12:05:37'),
+	(8, 'Mr. Tyler Kemmer MD', 'female', '2956767323904', '1979-04-17', 'mariam79@example.org', '1978-06-23 05:21:21', NULL, NULL),
+	(9, 'Pablo Durgan', 'male', '6925837007287', '2023-11-10', 'vkihn@example.net', '1991-04-08 15:12:15', NULL, NULL),
+	(10, 'Rodrick Morissette', 'female', '3158467387744', '1972-07-25', 'amanda33@example.com', '1994-08-09 05:11:03', NULL, NULL),
+	(11, 'Iacob Alexandru-Georgian', 'male', '1111111111111', '2024-10-09', 'iacobalex11@yahoo.com', '2024-10-26 13:34:07', '2024-10-26 13:36:31', '2024-10-26 13:36:31');
 
 -- Dumping structure for table totem_contact.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
@@ -60,11 +57,53 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `time` int NOT NULL,
   `batch` int unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Dumping data for table totem_contact.migrations: ~1 rows (approximately)
 INSERT INTO `migrations` (`id`, `version`, `class`, `group`, `namespace`, `time`, `batch`) VALUES
-	(1, '2024-10-25-102938', 'App\\Database\\Migrations\\CreateContactsTable', 'default', 'App', 1729930270, 1);
+	(4, '2024-10-25-102938', 'App\\Database\\Migrations\\CreateContactsTable', 'default', 'App', 1729941888, 1),
+	(5, '2024-10-26-112042', 'App\\Database\\Migrations\\CreatePhonesTable', 'default', 'App', 1729941888, 1);
+
+-- Dumping structure for table totem_contact.phones
+CREATE TABLE IF NOT EXISTS `phones` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `contact_id` int unsigned NOT NULL,
+  `phone_number` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `phones_contact_id_foreign` (`contact_id`),
+  CONSTRAINT `phones_contact_id_foreign` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Dumping data for table totem_contact.phones: ~24 rows (approximately)
+INSERT INTO `phones` (`id`, `contact_id`, `phone_number`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 1, '1488267593', '2012-02-10 13:15:59', NULL, NULL),
+	(2, 1, '9615069231', '2004-02-11 04:20:33', NULL, NULL),
+	(3, 1, '6639829371', '2012-07-26 10:45:59', NULL, NULL),
+	(4, 2, '1396833347', '2021-06-15 19:01:31', '2024-10-26 13:33:30', '2024-10-26 13:33:30'),
+	(5, 2, '2137316843', '1984-06-14 19:51:06', '2024-10-26 13:33:30', '2024-10-26 13:33:30'),
+	(6, 3, '4923544460', '1983-02-02 21:33:52', NULL, NULL),
+	(7, 3, '8572726776', '1992-01-30 13:25:10', NULL, NULL),
+	(8, 3, '8725946322', '1971-01-29 06:43:52', NULL, NULL),
+	(9, 4, '3612328291', '1996-07-16 09:34:23', NULL, NULL),
+	(10, 4, '1067902169', '1971-05-05 15:02:25', NULL, NULL),
+	(11, 5, '6460267517', '1988-07-22 06:45:47', NULL, NULL),
+	(12, 5, '7754012807', '1978-09-14 17:56:31', NULL, NULL),
+	(13, 6, '4851830132', '2014-10-23 20:26:26', NULL, NULL),
+	(14, 6, '5301704689', '2017-07-19 00:33:09', NULL, NULL),
+	(15, 6, '4444682820', '2015-05-24 10:35:12', NULL, NULL),
+	(16, 7, '7022442593', '2020-12-20 14:23:39', '2024-10-26 13:33:14', '2024-10-26 13:33:14'),
+	(17, 8, '0851545775', '1979-08-04 17:57:53', NULL, NULL),
+	(18, 9, '8529599949', '1974-05-04 13:12:30', NULL, NULL),
+	(19, 9, '8355533816', '1983-10-06 04:57:40', NULL, NULL),
+	(20, 9, '0774695602', '2023-04-03 06:25:28', NULL, NULL),
+	(21, 10, '1835793748', '1973-11-19 02:20:38', NULL, NULL),
+	(22, 10, '8768688652', '2019-10-12 23:57:32', NULL, NULL),
+	(23, 10, '9061377919', '1989-05-01 10:37:26', NULL, NULL),
+	(24, 7, '2222222222', '2024-10-26 13:31:31', '2024-10-26 13:33:14', '2024-10-26 13:33:14'),
+	(25, 11, '0758848066', '2024-10-26 13:34:07', '2024-10-26 13:36:31', '2024-10-26 13:36:31');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
